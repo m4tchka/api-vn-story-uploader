d5778f45b7dd5a1128ce8e665a31c534d2457585
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-11-22 13:32:50 +0000

    add example mongo connection functions

--- main.go
+++ main.go
@@ -1,13 +1,37 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	// "go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
 	handleRequests()
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -24,7 +48,3 @@ func testPostArticles(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Test post endpoint hit")
 
 }
-uri := "mongodb+srv://" + url.QueryEscape(username) + ":" + 
-		url.QueryEscape(password) + "@" + cluster + 
-		"/?authSource=" + authSource +
-		"&authMechanism=" + authMechanism
