6ae47d9f958d1c508cc478fc4b5a29ab3205d17a
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-11-28 21:52:35 +0000

    Added comments to most of the API & remove redundant comments

--- controller/controller.go
+++ controller/controller.go
@@ -62,7 +62,7 @@ func AllArticles(w http.ResponseWriter, r *http.Request) { // Test endpoint that
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type") // Indicates that any request must have the header of "Content-Type"
 	//https://stackoverflow.com/questions/39507065/enable-cors-in-golang
 	w.WriteHeader(http.StatusOK)        // Give the response a header of "200 OK"
-	json.NewEncoder(w).Encode(articles) // Write to the body of the respnose, the articles defined above.
+	json.NewEncoder(w).Encode(articles) // Write to the body of the response, the articles defined above.
 }
 
 //	func TestPostArticles(w http.ResponseWriter, r *http.Request) {
@@ -70,35 +70,36 @@ func AllArticles(w http.ResponseWriter, r *http.Request) { // Test endpoint that
 //	}
 func GetSpecificScene(w http.ResponseWriter, r *http.Request) {
 	log.Println("------------- GET SPECIFIC SCENE FUNC TRIGGERED -----------------")
-	specificSceneId, err := strconv.Atoi(mux.Vars(r)["id"]) //Take the variable from the parameter of the request and store it in specificSceneId
+	specificSceneId, err := strconv.Atoi(mux.Vars(r)["id"]) //Take the variable from the parameter of the request and store it in specificSceneId after converting it to an integer
 	if err != nil {
 		panic(err)
 	}
 	var scene bson.M // Declare the variable scene, of type bson.M
 	// bson.M is a MongoDB-specific type that is useful when the specific order of keys in an object is NOT important. If it did matter, bson.D would be more suitable.
-	if err := Coll.FindOne(context.Background(), bson.M{"id": specificSceneId}).Decode(&scene); err != nil {
+	if err := Coll.FindOne(context.Background(), bson.M{"id": specificSceneId}).Decode(&scene); err != nil { // Find the first document within the collection coll, that matches the filter where id is the id from the request parameter.
+		// Then, store the document in the variable scene.
 		log.Fatal(err)
 	}
 	fmt.Println("Specific scene:", scene)
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json") // As before ...
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	json.NewEncoder(w).Encode(scene)
 }
 func GetAllScenes(w http.ResponseWriter, r *http.Request) {
 	log.Println("------------- GET ALL SCENES FUNC TRIGGERED -----------------")
-	cursor, err := Coll.Find(context.TODO(), bson.M{})
+	cursor, err := Coll.Find(context.TODO(), bson.M{}) // Find all documents in the collection (since the filter passed in is empty)
 	if err != nil {
 		log.Fatal(err)
 	}
-	var scenes []bson.M // Must not be bson.D
-	if err = cursor.All(context.TODO(), &scenes); err != nil {
+	var scenes []bson.M                                        // Must not be bson.D
+	if err = cursor.All(context.TODO(), &scenes); err != nil { // Cursor points to all documents that were found by the Coll.find above, loops through them, and store them in scenes
 		log.Fatal(err)
 	}
 	for _, scn := range scenes {
 		fmt.Println("Scene printed >>>", scn)
 	}
-	defer cursor.Close(context.TODO())
+	defer cursor.Close(context.TODO()) // Close the cursor as soon as the response is sent back.
 	/*
 		for cursor.Next(context.TODO()) {
 			var scene bson.M
@@ -108,51 +109,48 @@ func GetAllScenes(w http.ResponseWriter, r *http.Request) {
 			fmt.Println(scene)
 		}
 	*/
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json") // As before ...
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	json.NewEncoder(w).Encode(scenes)
-
 }
 func PostScene(w http.ResponseWriter, r *http.Request) {
 	log.Println("------------- POST SCENE FUNC TRIGGERED -----------------")
-	body, err := io.ReadAll(r.Body)
+	body, err := io.ReadAll(r.Body) // Look through the body of the request and store the content as an array of bytes in the body variable
 	if err != nil {
 		panic(err)
 	}
-	// log.Println(string(body))
 	var s m.SceneObj
-	err = json.Unmarshal(body, &s)
+	err = json.Unmarshal(body, &s) // Unmarshal the array of bytes into a custom SceneObj struct (from models)
 	if err != nil {
 		panic(err)
 	}
 	log.Println(s.Scene)
-	insertAScene(s)
-	w.Header().Set("Content-Type", "application/json")
+	insertAScene(s)                                    // Call the insertAScene function with the SceneObj variable
+	w.Header().Set("Content-Type", "application/json") // As before ...
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	json.NewEncoder(w).Encode(s)
 }
 func insertAScene(s m.SceneObj) {
-	insOneRes, err := Coll.InsertOne(context.Background(), s)
+	insOneRes, err := Coll.InsertOne(context.Background(), s) // Insert the SceneObj variable into the collection as a new document
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Successfully inserted:", insOneRes)
+	fmt.Println("Successfully inserted:", insOneRes) // Print the result of the insert (a special _id string)
 }
 func DeleteScene(w http.ResponseWriter, r *http.Request) {
 	log.Println("------------- DELETE SCENE FUNC TRIGGERED -----------------")
-	idToBeDeleted, err := strconv.Atoi(mux.Vars(r)["id"])
+	idToBeDeleted, err := strconv.Atoi(mux.Vars(r)["id"]) //Take the variable from the parameter of the request and store it in idToBeDeleted after converting it to an integer.
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("id: %d of type %T\n", idToBeDeleted, idToBeDeleted)
-	deleteAScene(idToBeDeleted)
-	w.Header().Set("Content-Type", "application/json")
+	deleteAScene(idToBeDeleted)                        // Call the deleteAScene function with the integer id
+	w.Header().Set("Content-Type", "application/json") // As before ...
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 }
-func deleteAScene(id int) {
+func deleteAScene(id int) { // TODO: NOT IMPLEMENTED YET
 	var scene bson.M
 	if err := Coll.FindOne(context.Background(), bson.M{"id": id}).Decode(&scene); err != nil {
 		log.Fatal(err)
--- router/router.go
+++ router/router.go
@@ -10,16 +10,15 @@ import (
 
 func HandleRequests() *mux.Router {
 	log.Println("Server running ...")
-	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter := mux.NewRouter().StrictSlash(true) // Define a new router from mux (and allow trailing slashes for routes)
 	//TODO: TEST ROUTES ONLY -----------
-	myRouter.HandleFunc("/", ctl.HomePage)
-	myRouter.HandleFunc("/articles", ctl.AllArticles).Methods("GET")
-	// myRouter.HandleFunc("/articles", ctl.TestPostArticles).Methods("POST")
+	myRouter.HandleFunc("/", ctl.HomePage)                           // Route to the home page
+	myRouter.HandleFunc("/articles", ctl.AllArticles).Methods("GET") // Test route that displays some article structs
 	// --------------------------
-	myRouter.HandleFunc("/scenes/{id}", ctl.GetSpecificScene).Methods("GET", "OPTIONS")
-	myRouter.HandleFunc("/scenes", ctl.GetAllScenes).Methods("GET", "OPTIONS")
-	myRouter.HandleFunc("/scenes", ctl.PostScene).Methods("POST", "OPTIONS")
-	myRouter.HandleFunc("/scenes/{id}", ctl.DeleteScene).Methods("DELETE", "OPTIONS")
-	return myRouter
+	myRouter.HandleFunc("/scenes/{id}", ctl.GetSpecificScene).Methods("GET", "OPTIONS") // Route that takes an id parameter and returns the scene with that id
+	myRouter.HandleFunc("/scenes", ctl.GetAllScenes).Methods("GET", "OPTIONS")          // Route that returns all scenes within the collection
+	myRouter.HandleFunc("/scenes", ctl.PostScene).Methods("POST", "OPTIONS")            // Route that posts a new scene to the collection
+	myRouter.HandleFunc("/scenes/{id}", ctl.DeleteScene).Methods("DELETE", "OPTIONS")   // Route that deletes a single scene whose id matches the parameter from the collection
+	return myRouter                                                                     // Return this router so the function can be called and the router served from main.go
 
 }
